92b5b213755fe612e009c3698bf9a399c1b5dedc
jannisjr <joe_hannis@hotmail.com>
2023-08-25 11:48:21 +0100

    mongodb fix

--- api/bin/www.js
+++ api/bin/www.js
@@ -11,6 +11,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+console.log("DB_USER", process.env["DB_USER"]);
+console.log("DB_PASSWORD", process.env["DB_PASSWORD"]);
+console.log("DB_HOST", process.env["DB_HOST"]);
+console.log("DB_PORT", process.env["DB_PORT"]);
+console.log("DB_NAME", process.env["DB_NAME"]);
+
 const mongoDbUrl = `mongodb://${process.env["DB_USER"]}:${process.env["DB_PASSWORD"]}@${process.env["DB_HOST"]}:${process.env["DB_PORT"]}/${process.env["DB_NAME"]}?authSource=admin`;
 mongoose.connect(mongoDbUrl, {
   useNewUrlParser: true,
