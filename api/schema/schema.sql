8cda9f7d53da89cbc60e285a482715753a7eba5a
Joe Hannis <joe_hannis@hotmail.com>
2023-11-08 12:24:55 +0000

    bugfix: update schema

--- api/schema/schema.sql
+++ api/schema/schema.sql
@@ -1,8 +1,8 @@
-IF TABLE DOES NOT EXIST "Taplist Integration"."auth_info" THEN
-    CREATE TABLE "Taplist Integration"."auth_info" (
-        "id" SERIAL PRIMARY KEY,
-        "venue" VARCHAR(255) NOT NULL,
-        "auth_token" VARCHAR(255) NOT NULL,
-    );
-END IF;
+IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'auth_info' AND table_schema = 'Taplist Integration') THEN 
+        CREATE TABLE "Taplist Integration"."auth_info" (
+            "id" SERIAL PRIMARY KEY,
+            "venue" VARCHAR(255) NOT NULL,
+            "auth_token" VARCHAR(255) NOT NULL
+        );
+    END IF;
 ```
