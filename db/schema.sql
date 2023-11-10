296807df27fd049879bba844d8f27258f179f988
Joe Hannis <joe_hannis@hotmail.com>
2023-11-10 17:19:22 +0000

    bugfix: change postgres init to Dockerfile rather than docker-compose

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,6 @@
-DO $$
-BEGIN
-  CREATE DATABASE "Taplist Integration" WITH OWNER = postgres;
-  CREATE TABLE IF NOT EXISTS auth_info (
-    id SERIAL PRIMARY KEY,
-    venue VARCHAR(255) NOT NULL,
-    auth_token VARCHAR(255) NOT NULL
-  );
-END $$;
-
-
-```
+CREATE DATABASE "Taplist Integration" WITH OWNER = postgres;
+CREATE TABLE IF NOT EXISTS auth_info (
+  id SERIAL PRIMARY KEY,
+  venue VARCHAR(255) NOT NULL,
+  auth_token VARCHAR(255) NOT NULL
+);
