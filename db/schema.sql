cc4e01bb365f90688c4d1c5e43f4654d57599a5e
Joe Hannis <joe_hannis@hotmail.com>
2023-11-10 22:16:04 +0000

    bugfix: change postgres init to Dockerfile rather than docker-compose

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
 ALTER ROLE postgres WITH PASSWORD 'postgres';
-CREATE DATABASE IF NOT EXISTS "taplist-keg-level-manager" WITH OWNER = postgres;
-CREATE TABLE IF NOT EXISTS auth_info (
+CREATE DATABASE "taplist-keg-level-manager" WITH OWNER = postgres;
+CREATE TABLE auth_info (
   id SERIAL PRIMARY KEY,
   venue VARCHAR(255) NOT NULL,
   auth_token VARCHAR(255) NOT NULL
