01dd590cf948aa31f28a7724575ba2e52643ec26
Joseph Hannis <joe_hannis@hotmail.com>
2023-11-15 16:15:12 +0000

    bugfix: update taps controller to handle new object format

--- api/controllers/tapsController.js
+++ api/controllers/tapsController.js
@@ -7,6 +7,8 @@ const tapsController = async (req, res) => {
     console.log(details.rows[0]);
 
     const auth_token = details.rows[0].auth_token;
+    console.log('this is auth_token from tapsController');
+    console.log(auth_token);
     const venue = details.rows[0].venue;
     const response = await fetch(
       `https://api.taplist.io/api/v1/venues/${venue}/taps`,
@@ -15,6 +17,8 @@ const tapsController = async (req, res) => {
       }
     );
     const data = await response.json();
+    console.log('this is data from tapsController');
+    console.log(data);
     res.status(200).json(data);
   } catch (err) {
     console.error('Error occurred while fetching taps:', err);
