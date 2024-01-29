00339f410812e4e7c673cecbafcbf5ee5cb7b7ba
joehannis <joe_hannis@hotmail.com>
2024-01-29 21:11:53 +0000

    fix rendering logic

--- frontend/src/common/fetchAuth.js
+++ frontend/src/common/fetchAuth.js
@@ -2,6 +2,7 @@ const fetchAuth = async () => {
   try {
     const response = await fetch('http://localhost:3000/auth');
     const data = await response.json();
+    console.log(data);
     return data;
   } catch (error) {
     console.error('Error:', error);
