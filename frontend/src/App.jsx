737d423f1527a0600bf7c061f71c97fc7352d0cb
joehannis <joe_hannis@hotmail.com>
2024-01-29 20:20:13 +0000

    bugfix: fix components order in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
       try {
         const response = await fetchAuth();
         console.log(response);
-        if (response.length > 0) {
+        if (response?.length > 0) {
           setIsAuthorised(true);
         }
       } catch (error) {
