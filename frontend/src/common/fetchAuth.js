9be51be476a8fbf8a5a7b2ccd3b1ff0d23080c2d
joehannis <joe_hannis@hotmail.com>
2024-01-29 19:56:58 +0000

    refactor authorisation

--- frontend/src/common/fetchAuth.js
+++ frontend/src/common/fetchAuth.js
@@ -2,7 +2,7 @@ const fetchAuth = async () => {
   try {
     const response = await fetch('http://localhost:3000/auth');
     const data = await response.json();
-    return data;
+    return data.rows;
   } catch (error) {
     console.error('Error:', error);
     throw error;
