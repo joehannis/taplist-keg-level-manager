ce835d8eac6b942ce61b1db2cf9a8d537a745b43
joehannis <joe_hannis@hotmail.com>
2024-01-29 20:32:44 +0000

    bugfix: revert to using router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AuthForm from './components/auth/AuthForm';
 import fetchAuth from './common/fetchAuth';
 import fetchTapData from './common/fetchTapData';
@@ -36,52 +37,64 @@ const App = () => {
   };
 
   return (
-    <div className='main-container'>
-      <div className='header'>
-        <div className='logo-container'>
-          <img className='logo' src='./logo.png' alt='Taplist Wizard' />
-          <h1 className='title'>Taplist Keg Level Manager</h1>
+    <BrowserRouter>
+      <div className='main-container'>
+        <div className='header'>
+          <div className='logo-container'>
+            <img className='logo' src='./logo.png' alt='Taplist Wizard' />
+            <h1 className='title'>Taplist Keg Level Manager</h1>
+          </div>
+          {isAuthorised ? (
+            <>
+              <div className='unit-container'>
+                <select value={unit} onChange={handleUnitChange}>
+                  <option value='metric'>Metric</option>
+                  <option value='us-imperial'>US Imperial</option>
+                  <option value='british-imperial'>British Imperial</option>
+                </select>
+              </div>
+              <div className='icon-container'>
+                <div className='venue'>
+                  <h5>
+                    {tapData
+                      ? tapData[0]?.current_keg?.beverage?.producer?.name
+                      : null}
+                  </h5>
+                  <img
+                    className='venue-logo'
+                    src={
+                      tapData
+                        ? tapData[0]?.current_keg?.beverage?.producer?.picture
+                            ?.thumbnail_url
+                        : null
+                    }
+                    alt='logo'
+                  />
+                </div>
+              </div>
+            </>
+          ) : null}
         </div>
-      </div>
-      {isAuthorised ? (
-        <>
-          <div className='unit-container'>
-            <select value={unit} onChange={handleUnitChange}>
-              <option value='metric'>Metric</option>
-              <option value='us-imperial'>US Imperial</option>
-              <option value='british-imperial'>British Imperial</option>
-            </select>
-          </div>
-          <div className='icon-container'>
-            <div className='venue'>
-              <h5>
-                {tapData
-                  ? tapData[0]?.current_keg?.beverage?.producer?.name
-                  : null}
-              </h5>
-              <img
-                className='venue-logo'
-                src={
-                  tapData
-                    ? tapData[0]?.current_keg?.beverage?.producer?.picture
-                        ?.thumbnail_url
-                    : null
-                }
-                alt='logo'
-              />
-            </div>
-          </div>
-          <TapContainer
-            tapData={tapData}
-            setTapData={setTapData}
-            unit={unit}
-            setUnit={setUnit}
+
+        <Routes>
+          <Route
+            path='/'
+            element={
+              isAuthorised ? (
+                <TapContainer
+                  tapData={tapData}
+                  setTapData={setTapData}
+                  unit={unit}
+                  setUnit={setUnit}
+                />
+              ) : (
+                <AuthForm issAuthorised={isAuthorised} />
+              )
+            }
           />
-        </>
-      ) : (
-        <AuthForm />
-      )}
-    </div>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
