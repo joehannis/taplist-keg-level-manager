2e678e4d28c6171f6fda8cb0876c3af4d92f8056
joehannis <joe_hannis@hotmail.com>
2024-02-27 11:21:12 +0000

    remove login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import AuthForm from './components/auth/AuthForm';
+// import AuthForm from './components/auth/AuthForm';
 import fetchAuth from './common/fetchAuth';
-import deleteAuth from './common/deleteAuth';
 import fetchTapData from './common/fetchTapData';
 import TapContainer from './components/tapContainer/tapContainer';
 
 const App = () => {
-  const [isAuthorised, setIsAuthorised] = useState(false);
+  // const [isAuthorised, setIsAuthorised] = useState(false);
   const [tapData, setTapData] = useState(null);
   const [unit, setUnit] = useState('metric');
 
@@ -17,7 +16,7 @@ const App = () => {
       try {
         const response = await fetchAuth();
         if (response?.message?.rowCount > 0) {
-          setIsAuthorised(true);
+          // setIsAuthorised(true);
           fetchTapData(setTapData);
         }
       } catch (error) {
@@ -27,89 +26,89 @@ const App = () => {
     fetchAuthData();
   }, []);
 
-  useEffect(() => {
-    const tapFetch = async () => {
-      try {
-        await fetchTapData(setTapData);
-      } catch (error) {
-        console.error('An error occurred:', error);
-      }
-    };
-    tapFetch();
-  }, [isAuthorised]);
+  // useEffect(() => {
+  //   const tapFetch = async () => {
+  //     try {
+  //       await fetchTapData(setTapData);
+  //     } catch (error) {
+  //       console.error('An error occurred:', error);
+  //     }
+  //   };
+  //   tapFetch();
+  // }, [isAuthorised]);
 
   const handleUnitChange = (e) => {
     setUnit(e.target.value);
   };
 
-  const logoutHandler = async () => {
-    try {
-      await deleteAuth();
-      setIsAuthorised(false);
-      setTapData(null);
-    } catch (error) {
-      console.error('An error occurred:', error);
-    }
-  };
+  // const logoutHandler = async () => {
+  //   try {
+  //     await deleteAuth();
+  //     setIsAuthorised(false);
+  //     setTapData(null);
+  //   } catch (error) {
+  //     console.error('An error occurred:', error);
+  //   }
+  // };
 
   return (
     <BrowserRouter>
       <div className='main-container'>
         <div className='header'>
-          <button className='logout' onClick={() => logoutHandler()}>
+          {/* <button className='logout' onClick={() => logoutHandler()}>
             {' '}
             {isAuthorised ? 'Logout' : 'Try Again'}
-          </button>
+          </button> */}
           <div className='logo-container'>
             <img className='logo' src='./logo.png' alt='Taplist Wizard' />
             <h1 className='title'>Taplist Keg Level Manager</h1>
           </div>
-          {isAuthorised ? (
-            <>
-              <div className='icon-container'>
-                <div className='venue'>
-                  <h5>
-                    {tapData
-                      ? tapData[0]?.current_keg?.beverage?.producer?.name
-                      : null}
-                  </h5>
-                  <img
-                    className='venue-logo'
-                    src={
-                      tapData
-                        ? tapData[0]?.current_keg?.beverage?.producer?.picture
-                            ?.thumbnail_url
-                        : null
-                    }
-                    alt='logo'
-                  />
-                </div>
-                <div className='unit-container'>
-                  <select value={unit} onChange={handleUnitChange}>
-                    <option value='metric'>Metric</option>
-                    <option value='us-imperial'>US Imperial</option>
-                    <option value='british-imperial'>British Imperial</option>
-                  </select>
-                </div>
+          {/* {isAuthorised ? ( */}
+          <>
+            <div className='icon-container'>
+              <div className='venue'>
+                <h5>
+                  {tapData
+                    ? tapData[0]?.current_keg?.beverage?.producer?.name
+                    : null}
+                </h5>
+                <img
+                  className='venue-logo'
+                  src={
+                    tapData
+                      ? tapData[0]?.current_keg?.beverage?.producer?.picture
+                          ?.thumbnail_url
+                      : null
+                  }
+                  alt='logo'
+                />
               </div>
-            </>
-          ) : null}
+              <div className='unit-container'>
+                <select value={unit} onChange={handleUnitChange}>
+                  <option value='metric'>Metric</option>
+                  <option value='us-imperial'>US Imperial</option>
+                  <option value='british-imperial'>British Imperial</option>
+                </select>
+              </div>
+            </div>
+          </>
+          {/* ) : null} */}
         </div>
 
         <Routes>
           <Route
             path='/'
             element={
-              isAuthorised ? (
-                <TapContainer
-                  tapData={tapData}
-                  setTapData={setTapData}
-                  unit={unit}
-                  setUnit={setUnit}
-                />
-              ) : (
-                <AuthForm setIsAuthorised={setIsAuthorised} />
-              )
+              // isAuthorised ? (
+              <TapContainer
+                tapData={tapData}
+                setTapData={setTapData}
+                unit={unit}
+                setUnit={setUnit}
+              />
+              // ) : (
+              //   <AuthForm setIsAuthorised={setIsAuthorised} />
+              // )
             }
           />
         </Routes>
