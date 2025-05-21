d2d9ea7a6b9917c1a897ef769b6caf04c83020b3
Joe Hannis <45033532+joehannis@users.noreply.github.com>
2025-05-21 13:15:19 +0100

    Delete api directory

--- api/routes/authRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.post('/', authController.createAuth);
-router.get('/', authController.getAuth);
-router.delete('/', authController.deleteAuth);
-
-module.exports = router;
--- api/routes/resetRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const resetController = require("../controllers/resetController");
-
-router.patch("/", resetController);
-
-module.exports = router;
